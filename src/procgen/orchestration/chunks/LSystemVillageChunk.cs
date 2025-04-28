13808fcf403baf60675fb1e4797b3eeae60477a9
Christopher Goddard <chrisjgoddard@gmail.com>
2025-04-28 12:02:50 +1000

    Batch process turtle interpretation of l-system output to scene
    - Terrain3D does not lend itself to directly providing a means to attach
      to nodes created in the Godot game engine, as the generation of a
      terrain mesh runs closer to the metal
    - So we need some way to work around this, by instead attaching output
      to a different node in the scene
    - Therefore we are interested in running the turtle over our output data
      and childing to a new node,
      RootNode3D/Layers/LSystemVillage/Chunk_x0_x1
    - In order to prevent issues with causing the main thread of the game
      engine to crash due to threading over too many different objects, we
      circumvent this potential pitfall by batching node creation and
      attaching to a pending output array for each chunk
    - We then flush this data to the pending target node in the scene
    
    An example output can be seen here [1]
    
    There are of course issues with this output
    
    - First, it is difficult to navigate to it! When it is instantiated in
      the game world, one has to really trundle over for half a minute to
      see the output. It might be worth finding a means to dynamically
      modify camera movement speed + set a higher default velocity,
      I'll look into that as a high priority todo.
    - Second, the towns are a bit too regular. A grid layout doesn't seem
      very organic to me. Additionally, there are too many buildings in
      straight lines + not enough intersections.
    - Third, the buildings are not trued up with the height of the terrain
      mesh properly. This shouldn't be _too_ difficult to fix, but may
      require finessing with an iota of care.
    
    I can't do much about the blue screen artefacts, these are a terrain3d /
    godot game engine problem and are not in scope to fix; I assume others
    are on the case for such. Regardless, for a proof of concept, polish is
    not necessary. Problems like this will doubtless be fixed as this part
    of the ecosystem matures.
    
    [1] https://www.youtube.com/watch?v=WCZmntuJO7s

--- src/procgen/orchestration/chunks/LSystemVillageChunk.cs
+++ src/procgen/orchestration/chunks/LSystemVillageChunk.cs
@@ -8,6 +8,8 @@ public class LSystemVillageChunk : LayerChunk<LSystemVillageLayer, LSystemVillag
 {
     List<Vector3> housePositions = new();
     Point gridOrigin;
+    private Node3D? _chunkParent; 
+    private readonly List<Node3D> _pendingHouses = new();
 
     const int GLOBAL_SEED = 12345; // TODO: make this configurable and/or random but stored in the database when finally hook this up to a backend.
     const int CHUNK_X_RANDOM = 73856093;
@@ -26,6 +28,31 @@ public class LSystemVillageChunk : LayerChunk<LSystemVillageLayer, LSystemVillag
     public void DebugDraw() {
 		GD.Print("LSystemVillageChunk DebugDraw");
 	}
+
+    Node3D GetChunkParent()
+    {
+        // Re-use it if we already made it
+        if (_chunkParent != null && GodotObject.IsInstanceValid(_chunkParent))
+            return _chunkParent;
+
+        // ❶ Get the SceneTree – works from non-Node classes too
+        var tree = (SceneTree)Engine.GetMainLoop();
+
+        // ❷ Find the node you placed in the editor
+        //     RootNode3D/Layers/LSystemVillage
+        var villageRoot = tree.Root
+            .GetNode<Node3D>("RootNode3D/Layers/LSystemVillage");
+
+        // ❸ Make a node for *this* chunk the first time we need it
+        _chunkParent = new Node3D
+        {
+            Name = $"Chunk_{index.x}_{index.y}"
+        };
+        villageRoot.AddChild(_chunkParent);
+
+        return _chunkParent;
+    }
+
 
     void Build()
     {
@@ -66,11 +93,15 @@ public class LSystemVillageChunk : LayerChunk<LSystemVillageLayer, LSystemVillag
 
         GD.Print("House positions: " + string.Join(", ", housePositions));
 
-        // // Now housePositions has the exact positions for houses in this chunk
-        // foreach (var pos in housePositions)
-        // {
-        //     PlaceHouseInstance(pos);
-        // }
+        GD.Print("Layer parent node name: " + layer.layerParent.Name);
+
+        // Now housePositions has the exact positions for houses in this chunk
+        foreach (var pos in housePositions)
+        {
+            QueueHouseInstance(pos);
+        }
+
+        FlushHousesToScene();
     }
 
     float GetHeightAt(Vector3 position)
@@ -78,12 +109,42 @@ public class LSystemVillageChunk : LayerChunk<LSystemVillageLayer, LSystemVillag
         return GeoGridLayer.instance.SampleHeightAt(position.X, position.Z);
     }
 
-    // void PlaceHouseInstance(Vector3 position)
-    // {
-    //     position.Y = GetHeightAt(position);
-    //     var houseScene = GD.Load<PackedScene>("res://src/scenes/l_system_prefabs/house.tscn");
-    //     var houseInstance = houseScene.Instantiate<Node3D>();
-    //     houseInstance.Position = position;
-    //     layer.layerParent.AddChild(houseInstance);
-    // }
+    void QueueHouseInstance(Vector3 position)
+    {
+        position.Y = GetHeightAt(position);
+
+        var houseScene = GD.Load<PackedScene>(
+            "res://src/scenes/l_system_prefabs/house.tscn");
+        var inst = houseScene.Instantiate<Node3D>();
+        inst.Position = position;
+
+        _pendingHouses.Add(inst); 
+    }
+
+    void FlushHousesToScene()
+    {
+        GD.Print("LSystemVillageChunk FlushHousesToScene");
+        if (_pendingHouses.Count == 0) return;
+
+        // Copy to Godot Array so it survives the lambda capture
+        var batch = new Godot.Collections.Array<Node3D>(_pendingHouses.Cast<Node3D>());
+
+        // Clear the list for the next build cycle
+        _pendingHouses.Clear();
+
+        // Schedule the whole batch to be added next frame
+        SceneTree tree = (SceneTree)Engine.GetMainLoop();
+        tree.CreateTimer(0)                           // 0-sec One-Shot Timer
+            .Connect("timeout",
+                Callable.From(() =>
+                {
+                    var parent = GetChunkParent();    // now definitely in tree
+                    GD.Print("Adding " + batch.Count + " houses to scene for parent " + parent.Name);
+                    foreach (Node3D houseNode in batch)
+                    {
+                        houseNode.Name = "House_" + houseNode.Position.ToString();
+                        parent.AddChild(houseNode);
+                    }
+                }));
+    }
 }
