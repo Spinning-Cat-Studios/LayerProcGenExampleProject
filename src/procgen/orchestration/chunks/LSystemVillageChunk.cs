93582122c5834361691c6d78feca8a5d30a44c95
Christopher Goddard <chrisjgoddard@gmail.com>
2025-04-28 10:40:01 +1000

    Ensure that village l-system strings are all unique
    - Define the idea of a GLOBAL_SEED, as well as CHUNK_X_RANDOM and
      CHUNK_Y_RANDOM
    - Use these values to come up with a random integer that is different
      for each village chunk
    - Create a 3 letter 'axiom' from the alphabet to initialise the l-system
      state machine
    - Reduce iterations to 3 and define as a constant LSYSTEM_ITERATIONS
    - Print the output
    
    This results in
    
    L-System Sequence: DAADAADADAADADAADA[DA[D[B]D[B]]DA[D[B]D[B]]]DAADA[DA[D[B]D[B]]DA[D[B]D[B]]]ADADAADAADAACAADAADADAADA
    L-System Sequence: ADADAADADAADAADADAADAADADAADADAADAADADAADADAADAADADAADA
    L-System Sequence: DAADAADADAADADAADA[DA[D[B]D[B]]DA[D[B]D[B]]]DAADA[DA[D[B]D[B]]DA[D[B]D[B]]]DAADAADADAADA
    L-System Sequence: ADADAADADAADAADADAADADAADAADADAADADAADAADADAADA
    L-System Sequence: ADADAADAADAACAADAADADAADADAADAADADAADADAADAADADAADA
    L-System Sequence: DAADA[DA[D[B]D[B]]DA[D[B]D[B]]]DAADA[DA[D[B]D[B]]DA[D[B]D[B]]]ADADAADADAADAADADAADADAADAADADAADA
    
    ^ all different strings
    
    One concern now is that some of these strings might result in buildings
    being placed on top of each other, i.e. no roads etc. Three potential
    ways to fix
    
    - Regularity / Constraints. Force 'B' to be in the axiom, hence ensuring
      [ and ] strings are generated in the first iteration
    - Rule modification. Change the rules to encourage spread to be more
      sparse
    - Adaptation. Alter the interpreter to track location of buildings, if
      there is a collision in terms of house placement then dynamically
      modify the output by interpolating a move OR by interpreting the
      output as "dense township" and either
    
      1) replacing a building with a multi-level building
      2) placing a building right next to the existing one
    
    Other considerations
    
    - We would generically want buildings to be placed at an angle in
      alignment with the road or with neighbouring buildings
    - We want to choose our rules so that things look "organically like
      cities"
    
    What does a city / village look like?
    
    - Dense in the centre
    - Sparse towards the edges
    
    Emergent behaviour of l-systems _should_ provide this, but it is unclear
    whether such is automatic, or one needs to apply constraints to rules.
    Hence rudimentary thinking above.

--- src/procgen/orchestration/chunks/LSystemVillageChunk.cs
+++ src/procgen/orchestration/chunks/LSystemVillageChunk.cs
@@ -2,50 +2,74 @@ using Runevision.Common;
 using Runevision.LayerProcGen;
 using Godot;
 using System.Collections.Generic;
+using System.Linq;
 
 public class LSystemVillageChunk : LayerChunk<LSystemVillageLayer, LSystemVillageChunk>
 {
     List<Vector3> housePositions = new();
     Point gridOrigin;
 
+    const int GLOBAL_SEED = 12345; // TODO: make this configurable and/or random but stored in the database when finally hook this up to a backend.
+    const int CHUNK_X_RANDOM = 73856093;
+    const int CHUNK_Y_RANDOM = 19349663;
+    const int LSYSTEM_ITERATIONS = 3;
+
     public override void Create(int level, bool destroy)
     {
         GD.Print("LSystemVillageChunk Create");
-        // if (destroy)
-        //     housePositions.Clear();
-        // else
-        //     Build();
+        if (destroy)
+            housePositions.Clear();
+        else
+            Build();
     }
 
     public void DebugDraw() {
 		GD.Print("LSystemVillageChunk DebugDraw");
 	}
 
-    // void Build()
-    // {
-    //     gridOrigin = index * layer.chunkW;
+    void Build()
+    {
+        gridOrigin = index * layer.chunkW;
 
-    //     // Generate your L-system string
-    //     var rules = new Dictionary<char, string>
-    //     {
-    //         {'A', "ADA"},
-    //         {'B', "D[B]D[B]"},
-    //         {'C', "ACA"},
-    //         {'D', "DA"}
-    //     };
-    //     var lSystem = new LSystem("B", rules);
-    //     string lSequence = lSystem.Generate(4);
+        // Generate your L-system string
+        var rules = new Dictionary<char, string>
+        {
+            {'A', "ADA"},
+            {'B', "D[B]D[B]"},
+            {'C', "ACA"},
+            {'D', "DA"}
+        };
 
-    //     // Start interpreting at the chunk's origin
-    //     var interpreter = new TurtleInterpreter(GetHeightAt);
-    //     interpreter.Interpret(lSequence, gridOrigin.ToVector3(), Vector3.Forward, housePositions);
+        // Mix in chunk coords so each chunk seed varies
+        int chunkSeed = GLOBAL_SEED
+                    + index.x * CHUNK_X_RANDOM
+                    + index.y * CHUNK_Y_RANDOM;
+        
+        var rnd = new System.Random(chunkSeed);
+        var alphabet = rules.Keys.ToArray();
 
-    //     // Now housePositions has the exact positions for houses in this chunk
-    //     foreach (var pos in housePositions)
-    //     {
-    //         PlaceHouseInstance(pos);
-    //     }
-    // }
+        // Build a 3‑char axiom
+        string axiom = string.Concat(
+            Enumerable.Range(0, 3)
+                    .Select(_ => alphabet[rnd.Next(alphabet.Length)])
+        );
+
+        // Generate the L-system sequence
+        var lSystem = new LSystem(axiom, rules);
+        string lSequence = lSystem.Generate(LSYSTEM_ITERATIONS);
+
+        GD.Print("L-System Sequence: " + lSequence);
+
+        // // Start interpreting at the chunk's origin
+        // var interpreter = new TurtleInterpreter(GetHeightAt);
+        // interpreter.Interpret(lSequence, gridOrigin.ToVector3(), Vector3.Forward, housePositions);
+
+        // // Now housePositions has the exact positions for houses in this chunk
+        // foreach (var pos in housePositions)
+        // {
+        //     PlaceHouseInstance(pos);
+        // }
+    }
 
     // float GetHeightAt(Vector3 position)
     // {
