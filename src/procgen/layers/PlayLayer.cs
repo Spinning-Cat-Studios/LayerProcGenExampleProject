cfc034343e414c612cd1cb81a90aa01566bdc15d
Christopher Goddard <chrisjgoddard@gmail.com>
2025-06-03 06:45:24 +1000

    Ensure that we pass layerArguments to terrain lod manager

--- src/procgen/layers/PlayLayer.cs
+++ src/procgen/layers/PlayLayer.cs
@@ -3,14 +3,12 @@ using Godot;
 using Runevision.LayerProcGen;
 using Runevision.Common;
 using System;
-using System.Collections.Generic;
+using Godot.Collections;
 
 public class PlayLayer : ChunkBasedDataLayer<PlayLayer, PlayChunk, LayerService>, ILayerWithArguments
 {
     public override int chunkW => 8;
     public override int chunkH => 8;
-
-    private static bool _isVillageLayerInitialized = false;
 
     public PlayLayer() { }
 
@@ -20,19 +18,30 @@ public class PlayLayer : ChunkBasedDataLayer<PlayLayer, PlayChunk, LayerService>
         InitializePlayLayer(layerArguments);
     }
 
+    private void ConstructLandscapeLayerDependency(
+        LayerArgumentDictionary layerArguments,
+        Type landscapeLayerType,
+        int width,
+        int height)
+    {
+        var landscapeLayerArgs = layerArguments.Clone();
+        // Add "landscape_layer_id": "A" through "landscape_layer_id": "D" to the layer arguments
+        landscapeLayerArgs.parameters["landscape_layer_id"] = new Dictionary<string, Variant>
+        {
+            { "id", landscapeLayerType.Name }
+        };
+        var landscapeLayerInstance = (AbstractChunkBasedDataLayer)Activator.CreateInstance(landscapeLayerType, landscapeLayerArgs);
+        AddLayerDependency(new LayerDependency(landscapeLayerInstance, width, height));
+    }
+
     private void InitializePlayLayer(LayerArgumentDictionary layerArguments)
     {
         TerrainBlackboard.Initialize(new NodePath("Controller/TerrainLODManager/Terrain3D"));
 
-        var landscapeLayerA = LandscapeLayerA.GetInstance(layerArguments);
-        var landscapeLayerB = LandscapeLayerB.GetInstance(layerArguments);
-        var landscapeLayerC = LandscapeLayerC.GetInstance(layerArguments);
-        var landscapeLayerD = LandscapeLayerD.GetInstance(layerArguments);
-
-        AddLayerDependency(new LayerDependency(landscapeLayerD, 2048, 2048));
-        AddLayerDependency(new LayerDependency(landscapeLayerC, 1024, 1024));
-        AddLayerDependency(new LayerDependency(landscapeLayerB, 512, 512));
-        AddLayerDependency(new LayerDependency(landscapeLayerA, 256, 256));
+        ConstructLandscapeLayerDependency(layerArguments, typeof(LandscapeLayerD), 2048, 2048);
+        ConstructLandscapeLayerDependency(layerArguments, typeof(LandscapeLayerC), 1024, 1024);
+        ConstructLandscapeLayerDependency(layerArguments, typeof(LandscapeLayerB), 512, 512);
+        ConstructLandscapeLayerDependency(layerArguments, typeof(LandscapeLayerA), 256, 256);
 
         var villageLayer = LSystemVillageLayer.GetInstance(layerArguments);
 
@@ -45,7 +54,10 @@ public class PlayLayer : ChunkBasedDataLayer<PlayLayer, PlayChunk, LayerService>
             {
                 void Handler()
                 {
-                    _isVillageLayerInitialized = true;
+                    if (!LandscapeChunkCounterBlackboard.LandscapeChunksAreReady)
+                    {
+                        return;
+                    }
                     villageLayer.EnsureLoadedInBounds(bounds, level, levelData);
                     GD.Print("LSystemVillageLayer dependency loaded after LandscapeChunksReady signal.");
                 }
@@ -53,7 +65,7 @@ public class PlayLayer : ChunkBasedDataLayer<PlayLayer, PlayChunk, LayerService>
                 SignalBus.Instance.LandscapeChunksReady += Handler;
 
                 // Check if already ready, and manually trigger if so
-                if (LandscapeChunkCounterBlackboard.LandscapeChunksAreReady && !_isVillageLayerInitialized)
+                if (LandscapeChunkCounterBlackboard.LandscapeChunksAreReady)
                     Handler();
             }
         ));
